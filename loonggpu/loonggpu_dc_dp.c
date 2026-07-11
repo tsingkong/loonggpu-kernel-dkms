89e45547960ba4a502d5ee373f260bda05901f90
Xi Ruoyao <xry111@xry111.site>
2026-07-11 22:29:30 +0800

    loonggpu-bridge: make some functions static
    They are only used in the TU where the function is defined.  Thus we
    can make them static to fix some missing prototype warnings.
    
    Signed-off-by: Xi Ruoyao <xry111@xry111.site>
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>

--- loonggpu/loonggpu_dc_dp.c
+++ loonggpu/loonggpu_dc_dp.c
@@ -1192,7 +1192,7 @@ int ls2k3000_dp_resume(struct loonggpu_dc_crtc *crtc, int intf)
 	return 0;
 }
 
-int ls2k3000_dp_aux_detect_status(struct loonggpu_dc_crtc *crtc, int intf, struct connector_resource *resouce)
+static int ls2k3000_dp_aux_detect_status(struct loonggpu_dc_crtc *crtc, int intf, struct connector_resource *resouce)
 {
 	struct loonggpu_device *adev = crtc->dc->adev;
 	unsigned int detect_flag = 1;
